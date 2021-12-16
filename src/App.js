0b61ede35fbbdb89f64ae4af3c7e89a4b7dd6519
zakpayne8283 <zakpayne8283@gmail.com>
2021-12-16 11:39:22 -0800

    Added basic board component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
+import Board from "./components/Board/Board"; // TODO: Make this not Board/Board
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board/Board.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Board(props) {
+  return <div>This is the board</div>;
+}
