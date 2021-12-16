c79a9cc9760a33cf51086c8fc8076672492df0a8
zakpayne8283 <zakpayne8283@gmail.com>
2021-12-16 11:57:02 -0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Board from "./components/Board/Board"; // TODO: Make this not Board/Board
 
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   const [appState, setAppState] = useState({ museum: "AIC" });
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+// My Components
 import AIC from "../AIC/AIC";
 
+// Bootstrap
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export default function Board(props) {
-  return <div>{props.museum === "AIC" && <AIC />}</div>;
+  return (
+    <Container>
+      <Row>{props.museum === "AIC" && <AIC />}</Row>
+    </Container>
+  );
 }
