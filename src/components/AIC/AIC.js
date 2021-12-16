3f0ad39011b2835e998591761b69912b045f502a
Zak Payne <zakpayne8283@gmail.com>
2021-12-16 13:11:58 -0800

    Corrected Data Fetch

--- src/components/AIC/AIC.js
+++ src/components/AIC/AIC.js
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function AIC(props) {
-  const [artData, setArtData] = useState(false);
+  const [artData, setArtData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [dataError, setDataError] = useState(false);
 
-  fetch("https://api.artic.edu/api/v1/artworks/search?limit=5")
-    .then((res) => res.json())
-    .then(
-      (result) => {
-        setArtData(result);
-        setIsLoaded(true);
-      },
-      (error) => {
-        setDataError(error);
-        setIsLoaded(true);
-      }
-    );
+  useEffect(() => {
+    fetch("https://api.artic.edu/api/v1/artworks/search?limit=5&fields=id,image_id,title,artist_title")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setArtData(result);
+          setIsLoaded(true);
+        },
+        (error) => {
+          setDataError(error);
+          setIsLoaded(true);
+        }
+      );
+  }, [])
 
   if (dataError) {
     return <div>Error: {dataError.message}</div>;
@@ -24,11 +26,16 @@ export default function AIC(props) {
     return <div>Loading...</div>;
   } else {
     return (
-      <ul>
-        {artData.map((item) => (
-          <li>{JSON.stringify(item)}</li>
+      <div>
+        {artData.data.map((item) => (
+          // <li>{JSON.stringify(item)}</li>
+          <div>
+            <img src={"https://www.artic.edu/iiif/2/" + item.image_id + "/full/843,/0/default.jpg"} alt={item.title} />
+            <p>{item.title}</p>
+            <p>{item.artist_title}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     );
   }
 }
