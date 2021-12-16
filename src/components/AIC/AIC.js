d643123cae0c2f43e6b6a1dfa8a0f412b8397987
zakpayne8283 <zakpayne8283@gmail.com>
2021-12-16 12:06:30 -0800

    Added Basic Fetch

--- src/components/AIC/AIC.js
+++ src/components/AIC/AIC.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AIC(props) {
-  return <div>This is the Art Institute of Chicago page</div>;
+  const [artData, setArtData] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [dataError, setDataError] = useState(false);
+
+  fetch("https://api.artic.edu/api/v1/artworks/search?limit=5")
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        setArtData(result);
+        setIsLoaded(true);
+      },
+      (error) => {
+        setDataError(error);
+        setIsLoaded(true);
+      }
+    );
+
+  if (dataError) {
+    return <div>Error: {dataError.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {artData.map((item) => (
+          <li>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    );
+  }
 }
