91baa9d54a843c59cb35e1ae05cd59c20c82a7d4
Zak Payne <zakpayne8283@gmail.com>
2021-12-19 01:38:25 -0800

    Switched to Context

--- src/components/AIC/AIC.js
+++ src/components/AIC/AIC.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Masonry from 'react-masonry-css';
 
 import AICCard from "./AICCard";
+
+import { _AppContext } from "../../context";
 
 import Button from 'react-bootstrap/Button';
 
@@ -11,52 +13,47 @@ export default function AIC(props) {
   const [dataError, setDataError] = useState(false);
   const [dataPage, setDataPage] = useState(1);
 
+  const [ appState, dispatch ] = useContext(_AppContext);
+
   // Fetch Data
   useEffect(() => {
-    console.log(props);
 
-    let queryURL = '';
-    if (props.queryURL == false) {
-      queryURL = "https://api.artic.edu/api/v1/artworks/search?limit=25&page=1&fields=id,image_id,title,artist_title"
-    } else {
-      queryURL = props.queryURL + '&page=1';
-    }
-
-    fetch(queryURL)
+    fetch(appState.queryURL + "&page=" + appState.dataPage)
       .then((res) => res.json())
       .then(
         (result) => {
-          setArtData(result.data);
-          setIsLoaded(true);
-          setDataPage(dataPage + 1);
+
+          let newData = [];
+          if (appState.dataPage == 1) {
+            newData = result.data;
+          } else {
+            newData = [...appState.artData, ...result.data];
+          }
+          // Dispatch updates
+          dispatch({ type: 'artData', artData: newData });
+          dispatch({ type: 'dataLoaded', dataLoaded: true });
         },
         (error) => {
-          setDataError(error);
-          setIsLoaded(true);
+          dispatch({ type: 'errorMessage', errorMessage: error});
+          dispatch({ type: 'dataLoaded', dataLoaded: true });
         }
       );
-  }, [props.queryURL]);
+  }, [appState.dataPage, appState.queryURL]);
 
   const loadMore = () => {
-    fetch("https://api.artic.edu/api/v1/artworks/search?limit=25&page=" + dataPage + "&fields=id,image_id,title,artist_title")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setArtData([...artData, ...result.data]);
-          setIsLoaded(true);
-          setDataPage(dataPage + 1);
-        },
-        (error) => {
-          setDataError(error);
-          setIsLoaded(true);
-        }
-      );
+    dispatch({ type: 'dataPage', dataPage: dataPage + 1});
+  }
+
+  const showButton = () => {
+    if (appState.artData.length == (25 * appState.dataPage)) {
+      return <Button onClick={loadMore}>Load More</Button>
+    }
   }
 
   // Return error message on error
-  if (dataError) {
+  if (appState.errorMessage != false) {
     return <div>Error: {dataError.message}</div>;
-  } else if (!isLoaded) {
+  } else if (!appState.dataLoaded) {
     // Loading data
     return <div>Loading...</div>;
   } else {
@@ -67,11 +64,11 @@ export default function AIC(props) {
           className="masonry-grid"
           columnClassName="masonry-grid_column"
         >
-          {artData.map((item) => (
+          {appState.artData.map((item) => (
             <AICCard item={item} />
           ))}
         </Masonry>        
-        <Button onClick={loadMore}>Load More</Button> {/* TODO: Change this to infinity scroll */}
+        {showButton()}
       </>
     );
   }
